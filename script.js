49d7662e323a6c1c194df69c866749ffb72c9028
Muksam <Muksam@example.com>
2022-10-24 16:42:50 +0545

    change in index.html

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Engineer","Game Developer", "Data-Scientist"],
+        strings: ["Python Developer","Game Developer", "Data-Scientist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Engineer","Game Developer", "Data-Scientist"],
+        strings: ["Python Developer","Game Developer", "Data-Scientist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
